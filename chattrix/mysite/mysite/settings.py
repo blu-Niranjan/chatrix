f1e351285e7ea39d6e56a4bf0c0af7a5cbd893cf
Niranjan <malakaniranjan080@gmail.com>
2025-06-11 15:47:20 +0530

    Update settings.py

--- chattrix/mysite/mysite/settings.py
+++ chattrix/mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$+4_%r1mn@2)$7hc+r_8aup@+4f_)22e%x5bd(elvfxqt_2&*_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['chatrix.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
